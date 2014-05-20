d59e4768a3be9e20ef8251510967e2c1bbdb4513
Salem Hilal <salemhilal@gmail.com>
2014-05-19 21:19:30 -0400

    Error messages, changelog, etc.

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+Changelog
+---------
+
+v0.3.0
+======
+
+ * Added error messages on unsuccessful formats
+ * Added this changelog, with plans to retroactively add old changes. 
--- main.js
+++ main.js
@@ -2,7 +2,8 @@ Jsfmt = require('./lib/jsfmt');
 
 module.exports = {
   configDefaults: {
-    pathToJsfmt: 'jsfmt' // Where the jsfmt binary is located
+    pathToJsfmt: 'jsfmt', // Where the jsfmt binary is located
+    showErrors: true, // Whether or not to show the error bar
   },
   activate: function() {
     Jsfmt.start();
